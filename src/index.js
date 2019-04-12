8418c284baf9edd51d74fe0a49bf48db0658f8df
Calos <inoue.sora@gmail.com>
2019-04-12 18:16:14 -0500

    WP-2 0. Added loaders for css and images support.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 import _ from 'lodash';
 
+import Logo from './react-icon.png';
+
 
 function component() {
   let element = document.createElement('div');
 
   element.innerHTML = _.join(['Hello ', 'webpack'], '');
+  element.classList.add('hello');
+
+  // Add the image to our existing div.
+  let reactLogo = new Image();
+  reactLogo.src = Logo;
+
+  element.appendChild(reactLogo);
 
   return element;
 }
